eaa03d55b13c1af85fc04b203e3e6122c278616d
github username <github email address>
2019-10-28 16:30:40 -0400

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,23 @@
-// your Bomb code here!
+import React, { Component } from 'react'
+import ImageSlider from './ImageSlider'
+
+export default class Bomb extends Component { 
+
+    constructor(props){
+        super(props);
+        console.log(this.props.initialCount)
+        this.state = { 
+            secondsLeft: this.props.initialCount
+        }
+    }
+
+    render() {
+        if(this.state.secondsLeft > 0){
+            return <p>{this.state.secondsLeft} seconds left before I go boom!</p>
+        } 
+        else { 
+            return <p>Boom!</p>
+        }      
+    }
+
+}
